252bcb9dc43ae6fd7f1e8b6e0ce4998d7262863c
Lunarian-Nova <ckeister@anamosa.k12.ia.us>
2026-05-13 15:31:22 +0000

    Lmao

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function guessNumber(guess) {
+  if (guess != 25) {
+    return 'not it';
+  }
+  return 'you got it';
+}
+
+function strictGuessNumber(guess) {
+  if (guess !== 25) {
+    return 'not it';
+  }
+  return 'you got it';
+}
 
